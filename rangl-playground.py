dc57221fd17a17f3fb29357a16df9059d8a16ef0
Simon Tindemans <s.h.tindemans@tudelft.nl>
2021-01-19 09:54:22 +0100

    refactoring

--- rangl-playground.py
+++ rangl-playground.py
@@ -16,13 +16,13 @@ logger.setLevel(logging.DEBUG)
 
 
 
-def train_rl(env, models_to_train=40, episodes_per_model=100):
+def train_SAC(env, models_to_train=1, episodes_per_model=100, **kwargs):
     """
     RL training function. 
     """
 
     # using SAC - adjusted gamma to a lower value due to the relatively fast response of the system
-    model = SAC(MlpPolicy, env, verbose=1, gamma=0.85)
+    model = SAC(MlpPolicy, env, **kwargs)
     start = time.time()
 
     for i in range(models_to_train):
@@ -36,13 +36,13 @@ def train_rl(env, models_to_train=40, episodes_per_model=100):
     # return final model
     return model
 
-def run_rl(model, env, plot_name):
+def run_episode(env, agent, plot_name = None):
     observation = env.reset()
     done = False
     while not done:
         # Specify the action. Check the effect of any fixed policy by specifying the action here:
         # note: using 'deterministic = True' for evaluation fixes the SAC policy
-        action, _states = model.predict(observation, deterministic=True)
+        action, _states = agent.predict(observation, deterministic=True)
         observation, reward, done, info = env.step(action)
     # plot the episode using the modified function (includes realisations at the bottom right)
     if plot_name is not None:
@@ -54,16 +54,16 @@ base_env = gym.make("reference_environment:reference-environment-v0")
 env = envwrapper.PlotWrapper(envwrapper.ActWrapper(envwrapper.EfficientObsWrapper(base_env, obs_length=25)))
 
 # Train an RL agent on the environment
-agent = train_rl(env, models_to_train=1, episodes_per_model=500)
+agent = train_SAC(env, episodes_per_model=10, verbose=1, gamma=0.9)
 
 # Perform two independent runs
-run_rl(agent, env, "agent_run_1.png")
-run_rl(agent, env, "agent_run_2.png")
+run_episode(env, agent, "agent_run_1.png")
+run_episode(env, agent, "agent_run_2.png")
 
 # collect results over 50 independent runs, display summary statistics
 result_list = np.zeros(50)
 for i in range(len(result_list)):
-    result_list[i] = run_rl(agent, env, None)
+    result_list[i] = run_episode(env, agent)
 
 print(f"Summary of 50 results:")
 print(f"Mean: {np.mean(result_list)}")
