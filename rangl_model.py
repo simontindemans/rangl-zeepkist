09c9f4a8480c31280f9a73c75ba3f68cc71a0089
Simon Tindemans <s.h.tindemans@tudelft.nl>
2021-01-21 00:34:07 +0100

    created standalone environment file

---
+++ rangl_model.py
@@ -0,0 +1,36 @@
+# FROM env.py - copy of model parameters
+
+class Parameters:
+    # (Avoid sampling random variables here: they would not be resampled upon reset())
+    # problem-specific parameters
+    imbalance_cost_factor_high = 50
+    imbalance_cost_factor_low = 7
+    ramp_1_max = 0.2
+    ramp_2_max = 0.5
+    ramp_1_min = -0.2
+    ramp_2_min = -0.5
+    generator_1_max = 3
+    generator_1_min = 0.5
+    generator_2_max = 2
+    generator_2_min = 0.5
+    generator_1_cost = 1
+    generator_2_cost = 5
+
+    # time parameters
+    steps_per_episode = 96
+    first_peak_time = 5
+
+
+class SkeletonEnvironment:
+
+    param = Parameters()  # parameters singleton
+
+    def __init__(self, forecast_length=None):
+        if forecast_length is None:
+            self.forecast_length = self.param.steps_per_episode
+        else:
+            self.forecast_length = forecast_length
+        return
+
+
+
